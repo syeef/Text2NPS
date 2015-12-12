28246c90ad65cdc6685713f057ca8c0c86ddc8cc
Manthan The <Manthan.Dave@capitalone.com>
2015-12-12 19:54:40 +0000

    Config added.

---
+++ conf.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lao345 on 12/12/15.
+ */
+var config = {
+
+};
+
+config.twilio = require('./conf.twilio.keys');
+
+module.exports = config;
---
+++ conf.twilio.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lao345 on 12/12/15.
+ */
+module.exports = {
+    accountSid: "",
+    authToken: ""
+};
